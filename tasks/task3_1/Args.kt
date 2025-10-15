47d2313910510c78ded6454d53c4644eae882a64
jhkd8999 <jhkd8999@leeds.ac.uk>
2025-10-15 14:28:00 +0000

    Completed tasks 3.1, 3.2, 3.3

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,10 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("Error: 2 arguments required")
+        exitProcess(1)
+    }
+    println(args[0])
+    println(args[1])
+}
